3fc08802720c4c403c9bf250d88be4b4f70488d4
Mathieu Duponchelle <mathieu.duponchelle@opencreed.com>
2015-12-10 18:44:29 +0100

    Make the nuke_and_build script configurable

--- nuke_and_build.sh
+++ nuke_and_build.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 rm -rf hotdoc-private static/html && \
-	hotdoc --output static/html run --conf-file ~/devel/test_hotdoc/hotdoc.json && \
-	python hotdoc_server.py run --conf-file ~/devel/test_hotdoc/hotdoc.json
+	hotdoc --output static/html --editing-server http://dev.example.com:5055 run --conf-file $1 && \
+	python hotdoc_server.py --output static/html --editing-server http://dev.example.com:5055 run --conf-file $1
